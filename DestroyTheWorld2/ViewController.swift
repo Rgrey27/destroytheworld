99a764ed38affc96d5f784b12e2e471d7d901b6b
Rafael <rcraft916@gmail.com>
2021-01-24 03:20:20 -0800

    Initial Commit

---
+++ DestroyTheWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld2
+//
+//  Created by RAFAEL CERVANTES on 1/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
